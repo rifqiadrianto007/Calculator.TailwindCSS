79ff87b5fba916519d8eb6e615e0c86a84045fed
rifqi_adrianto <146399773+rifqiadrianto007@users.noreply.github.com>
2024-10-30 09:01:14 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+        colors : {
+            primary : '#374151',
+            secondary : '#0ea5e9',
+        },
+    },
+  },
+  plugins: [],
+}
+
